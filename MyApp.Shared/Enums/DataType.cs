feb1fb332d776037889aa0b5593ad2b6b8ced179
David Llanes <david.llanes@smaco.biz>
2025-06-06 12:51:28 -0700

    first iteration

---
+++ MyApp.Shared/Enums/DataType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tapper;
+
+namespace SmacoDevs.MyShared.Enums
+{
+    [TranspilationSource]
+    public enum DataType
+    {
+        PositiveNegative = 1,
+        Numeric = 2,
+        Text = 3,
+        Percentage = 4,
+        MultiSelect = 5,
+        Date = 6
+    }
+
+    [TranspilationSource]
+    public enum MedicalNoteDataType
+    {
+        Text = DataType.Text,
+        MultiSelect = DataType.MultiSelect,
+        Date = DataType.Date
+    }
+}
---
+++ MyApp.Shared/Enums/Status.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tapper;
+
+namespace SmacoDevs.MyShared.Enums
+{
+    [TranspilationSource]
+    public enum Status
+    {
+        Inactive = 0,
+        Active = 1,
+        Deleted = 2,
+        Testing = 3
+    }
+}
